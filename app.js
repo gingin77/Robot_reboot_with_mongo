22f7da8466e4e3cb64664b13f9119bba0cf7819e
gingin77 <ginniehench@gmail.com>
2017-08-28 14:12:26 -0400

    things are showing up on the page, which are pulled from the mongo db

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
-const MongoClient = require('mongodb').MongoClient
-  , assert = require('assert')
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+  // , assert = require('assert')
 
 // Connection URL
-let mongoURL = 'mongodb://localhost:27017/myproject'
+let mongoURL = 'mongodb://localhost:27017/newdb'
 
 const app = express()
 app.engine('mustache', mustacheExpress())
 app.set('view engine', 'mustache')
 app.set('views', './views')
 
-// app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 // Use connect method to connect to the server
-MongoClient.connect(mongoURL, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  findDocuments(robots,)
-
-  db.close();
-})
-
+// MongoClient.connect(mongoURL, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+//   //
+//   // findDocuments(robots)
+//
+//   db.close();
+// })
 
 // What Clinton posted in below.. However, I've changed restaurants to robots and made other db-specific modifications.
 app.use('/', function (req, res) {
   MongoClient.connect(mongoURL, function (err, db) {
     const robots = db.collection('robots');
-    restaurants.find({}).toArray(function (err, docs) {
-      res.render("index", {restaurants: docs})
+    robots.find({}).toArray(function (err, docs) {
+      res.render('robot', {robots: docs})
     })
   })
 })
+
+app.get('/', function (req,res){
+  res.render('robot', dataset)
+  })
 
 // var findDocuments = function(db, callback) {
 //   // Get the documents collection
